07d1900b99e7e77b66d4d9e8dda6cad434376787
DOES <ldk980130@gmail.com>
2022-08-28 16:49:41 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/study/transactionseparation/TransactionSeparationApplication.java
@@ -0,0 +1,13 @@
+package com.study.transactionseparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSeparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSeparationApplication.class, args);
+	}
+
+}
